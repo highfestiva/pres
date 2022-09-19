9ac0285751f41c751f9bc85fda8f31bacee9a9b6
Jonte <highfestiva@gmail.com>
2022-09-19 12:10:23 +0200

    Add cursor compliance and more naviation

--- pres.py
+++ pres.py
@@ -29,7 +29,11 @@ class Editor:
     maxy, maxx = scr.getmaxyx()
     for sy, line in enumerate(self.lines[self.y:self.y+maxy]):
       scr.addstr(sy, 0, line[self.x:self.x+maxx])
-    scr.move(self.cy - self.y, self.cx - self.x)
+    # truncate cursor to end of line
+    linelen = len(self.lines[self.cy])
+    cx = min(self.cx, linelen)
+    x = max(0, cx - self.x)
+    scr.move(self.cy - self.y, x)
     scr.refresh()
   
   def handle_key(self, scr, k):
@@ -46,18 +50,36 @@ class Editor:
         self.scroll_lines(scr, -1)
     elif k == 'KEY_DOWN':
       self.cy = min(len(self.lines)-1, self.cy+1)
-      if self.cy >= self.y+lowest_y:
+      if self.cy >= self.y+maxy-2:
         self.scroll_lines(scr, +1)
+    elif k == 'kUP5': # Ctrl+Up
+      self.scroll_lines(scr, -1)
+    elif k == 'kDN5': # Ctrl+Down
+      self.scroll_lines(scr, +1)
     elif k == 'KEY_LEFT':
-      self.cx = max(0, self.cx-1)
+      self.cx = max(0, min(len(self.lines[self.cy])-1, self.cx-1))
       if self.cx <= self.x+1:
         self.scroll_rows(scr, -1)
     elif k == 'KEY_RIGHT':
       self.cx = min(len(self.lines[self.cy]), self.cx+1)
       if self.cx >= self.y+highest_x:
         self.scroll_rows(scr, +1)
+    elif k == 'KEY_HOME':
+      self.x = self.cx = 0
+    elif k == 'KEY_END':
+      self.cx = len(self.lines[self.cy])
+      self.x = max(0, self.cx-highest_x)
+    elif k == 'kHOM5':
+      self.x = self.cx = self.y = self.cy = 0
+    elif k == 'kEND5':
+      self.y = lowest_y
+      self.cy = len(self.lines) - 1
+      self.cx = len(self.lines[self.cy])
+      self.x = max(0, self.cx-highest_x)
     elif k == '\x04':
       raise KeyboardInterrupt()
+    else:
+      print('key:', k.encode())
 
   def scroll_rows(self, scr, dx):
     maxy, maxx = scr.getmaxyx()
@@ -77,16 +99,18 @@ class Editor:
       y = 0
     elif y >= lowest_y:
       if self.y == lowest_y and y_cursor:
-        if self.cy == lowest_y:
+        if self.cy == len(self.lines)-1:
           self.cx = len(self.lines[-1])
         else:
-          self.cy = lowest_y
+          self.cy = len(self.lines)-1
       y = lowest_y
 
     d = y - self.y
     self.y += d
     if y_cursor:
-      self.cy = min(lowest_y, max(0, self.cy+d))
+      self.cy = min(len(self.lines)-1, max(0, self.cy+d))
+    self.cy = min(self.y+maxy-1, self.cy)
+    self.cy = max(self.y, self.cy)
 
 
 def main():
